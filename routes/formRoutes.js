6da7342d831b67dbdb2cfa9ceb1ef3e349a40995
AKSHAY <axayjewelstudio@gmail.com>
2026-03-02 00:14:46 +0530

    Fixed validateFormData error

--- routes/formRoutes.js
+++ routes/formRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const formController = require('../controllers/formController');
 
-router.post('/form-submission', validateFormData, formController.handleFormSubmission);
+router.post('/form-submission', formController.handleFormSubmission);
 
 module.exports = router;
